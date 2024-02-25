60d61df187d8c4abdd7efe7d942a383a5b156b16
JKawa121 <jay.kawa092@svkmmumbai.onmicrosoft.com>
2024-02-25 21:51:26 +0530

    i will be careful next time

---
+++ trial.js
@@ -0,0 +1,8 @@
+// substring
+const d= new Date();
+function getSubstring(str, start, end) {
+    console.log("Original String:", str);
+    console.log("After substring:", str.substring(start, end));
+  }
+  getSubstring("Hello World", 0, 8);
+  console.log(d.getMonth());
